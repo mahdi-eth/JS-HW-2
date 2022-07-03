4f48fc520828a97b324a2124baa0587f5ce422d0
Mahdi EttehadNejad <mahdi.ettehad85@gmail.com>
2022-07-03 10:43:37 +0430

    commit message

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let age = prompt("enter your age , please...");
+age = parseInt(age) + 56;  
+console.log(`in fifty six years you'll be ${age}`)
+if (age >= 18 || age <= 20 ) {
+    console.log("dude, Why are you here Go get some work!");
+}
